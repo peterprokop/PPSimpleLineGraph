e7210b0fc5ad0f1ed56cf0db887fa834311e97f6
Peter Prokop <pitrowech@gmail.com>
2017-01-06 19:37:20 +0100

    Add framework target

---
+++ Sample Project/SimpleLineChart/SimpleLineChart.h
@@ -0,0 +1,19 @@
+//
+//  SimpleLineChart.h
+//  SimpleLineChart
+//
+//  Created by Peter Prokop on 06/01/17.
+//  Copyright © 2017 Boris Emorine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SimpleLineChart.
+FOUNDATION_EXPORT double SimpleLineChartVersionNumber;
+
+//! Project version string for SimpleLineChart.
+FOUNDATION_EXPORT const unsigned char SimpleLineChartVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleLineChart/PublicHeader.h>
+
+
